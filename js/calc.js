d0a1d2dc752a5d8a63afd533af53963bd8ff2f67
Djmage1 <dashalnaidoo1@gmail.com>
2022-06-15 20:20:53 +0200

    Assignment-4 v.2.0

--- js/calc.js
+++ js/calc.js
@@ -5,3 +5,19 @@ function operate() {
 
     document.getElementById("display").value = calculation;
 }
+
+function addition() {
+    document.getElementById("display").value += " + ";
+}
+
+function subtract() {
+    document.getElementById("display").value += " - ";
+}
+
+function multiply() {
+    document.getElementById("display").value += " * ";
+}
+
+function divide() {
+    document.getElementById("display").value += " / ";
+}
