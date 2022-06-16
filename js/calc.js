98883c6c553858959d449c20bc0cce9bdf3986d8
Djmage1 <dashalnaidoo1@gmail.com>
2022-06-16 12:31:15 +0200

    Assignment-4 v2.1

--- js/calc.js
+++ js/calc.js
@@ -1,23 +1,132 @@
-function operate() {
-    equation = document.getElementById("display").value;
-
-    calculation = eval(equation);
-
-    document.getElementById("display").value = calculation;
-}
+let displayText;
+let equation;
+let operand;
+let result;
+let num1;
+let num2;
 
 function addition() {
-    document.getElementById("display").value += " + ";
+    result = (num1 + num2);
+
+    document.getElementById("display").value = result;
 }
 
 function subtract() {
-    document.getElementById("display").value += " - ";
+    result = (num1 - num2);
+    
+    document.getElementById("display").value = result;
 }
 
 function multiply() {
-    document.getElementById("display").value += " * ";
+    result = (num1 * num2);
+
+    document.getElementById("display").value = result;
 }
 
 function divide() {
-    document.getElementById("display").value += " / ";
+    result = (num1 / num2);
+
+    document.getElementById("display").value = result;
+}
+
+function getOperand(buttonId) {
+    getNumbers();
+    operand = buttonId;
+    document.getElementById("display").value = "";
+}
+
+function getNumbers() {
+    equation = document.getElementById("display").value;
+
+    if(num1 == null) {
+        num1 = parseFloat(equation);
+    }
+    else if(num2 == null) {
+        num2 = parseFloat(equation);
+    }
+}
+
+function addDot() {
+    displayText = document.getElementById("display").value;
+
+    if(displayText.includes(".")) {
+        
+    }
+    else if(displayText == "") {
+        
+    }
+    else {
+        document.getElementById("display").value += ".";
+    }
+}
+
+function checkZero() {
+    displayText = document.getElementById("display").value;
+
+    if(displayText == null) {
+        
+    }
+    else {
+        document.getElementById("display").value += "0";
+    }
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+    operand = null;
+    num1 = null;
+    num2 = null;
+}
+
+function backspace() {
+    equation = document.getElementById("display").value;
+
+    document.getElementById("display").value = equation.slice(0, -1);
+}
+
+
+
+function operate() {
+    equation = document.getElementById("display").value;
+
+    if(equation == "" || equation == null) {
+
+    }
+    else {
+        getNumbers();
+        
+        switch(operand) {
+            case"+":
+                addition();
+                operand = null;
+                num1 = null;
+                num2 = null;
+                result = null;
+                break;
+            
+            case"-":
+                subtract();
+                operand = null;
+                num1 = null;
+                num2 = null;
+                result = null;
+                break;
+            
+            case"*":
+                multiply();
+                operand = null;
+                num1 = null;
+                num2 = null;
+                result = null;
+                break;
+            
+            case"/":
+                divide();
+                operand = null;
+                num1 = null;
+                num2 = null;
+                result = null;
+                break;
+        }
+    }
 }
